76efd914ef3abd323cbcd04a8a97d8fe775b09e9
den <denny1301@gmail.com>
2019-12-09 21:45:29 +0200

    Added home page layout

--- OnlineShop/urls.py
+++ OnlineShop/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from home.views import home_screen_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_screen_view, name='home'),
 ]
